b3401eae4cdaa326c56dd5f27f25ed52c308b7bf
Kisha Richardson <kisha@spunklabs.com>
2016-06-20 18:18:14 -0700

    lesson_16 updates to music_app

---
+++ 16_Project_Working_Session/my_music_app/app/models/song.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  length     :string
+#  genre      :string
+#  artist_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  lyric_url  :string
+#
+
+class Song < ActiveRecord::Base
+  belongs_to :artist
+end
