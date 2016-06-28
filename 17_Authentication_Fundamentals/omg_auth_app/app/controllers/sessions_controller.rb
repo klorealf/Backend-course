165f728b89ac4c229d44721e6653d174bcaa9f50
Kisha Richardson <kisha@spunklabs.com>
2016-06-27 19:32:43 -0700

    add authentication

--- 17_Authentication_Fundamentals/omg_auth_app/app/controllers/sessions_controller.rb
+++ 17_Authentication_Fundamentals/omg_auth_app/app/controllers/sessions_controller.rb
@@ -1,5 +1,21 @@
 class SessionsController < ApplicationController
   def create
-    render text: request.env['omniauth.auth'].to_json
+    begin
+      auth_hash = request.env['omniauth.auth']
+      @user = User.from_omniauth(auth_hash)
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.name}"
+    rescue
+      flash[:warning] = "There was an error"
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    if current_user
+      session.delete[:user_id]
+      flash[:success] = "You are Logged Out!"
+    end
+    redirect_to root_path
   end
 end
--- 17_Authentication_Fundamentals/omg_auth_app/config/routes.rb
+++ 17_Authentication_Fundamentals/omg_auth_app/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  #dyanmic route for authentication providers
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy', as: 'signout'
+
   get 'test', to: 'static_pages#test', as: 'test'
   root 'static_pages#home'
 end
